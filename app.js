128303fb26a15901d17d6a0d51fc3240da73cbb5
Aditya Agarwal <56028864+adityaagarwal1710@users.noreply.github.com>
2020-09-12 14:31:33 +0530

    Check this link: https://still-gorge-81551.herokuapp.com/
    Added Heroku services to host the website on the internet.

--- app.js
+++ app.js
@@ -73,6 +73,6 @@ app.post("/failure",function(req,res){
     res.redirect("/");
 })
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Port creted on 3000 and is running");
 })
